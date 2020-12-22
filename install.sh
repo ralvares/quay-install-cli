a990bb4161c09ac80d5d8e3121a14b457b9648c9
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-22 17:08:27 +0100

    housekeeping

--- install.sh
+++ install.sh
@@ -5,31 +5,30 @@
 echo "Creating Quay namespace"
 oc apply -f quay-namespace.yaml
 echo "Quay namespace created!"
-
+echo " "
 echo "Creating Quay OperatorGroup"
 oc apply -f quay-og.yaml
 echo "Quay OperatorGroup created!"
-
-echo "Creating Quay Subscription"
-oc apply -f quay-sub.yaml
-echo "Quay Subscription created!"
-wait 5
-
+echo " "
 echo "Creating Quay pull secret"
 oc apply -f quay-secret.yaml
 echo "Quay secret created!"
-
+echo " "
+echo "Creating Quay Subscription"
+oc apply -f quay-sub.yaml
+echo "Quay Subscription created!"
+echo " "
 echo "Deploying QuayEcosystem CR"
 HOSTNAME=$(oc config view --minify -o jsonpath='{.clusters[*].cluster.server}' | rev | cut -d':' -f2 | rev | cut -b 6-)
 sed -i -e "s|.cluster-cd57.cd57.example.opentlc.com|$HOSTNAME|g" quay-cr.yaml
 oc apply -f quay-cr.yaml
-echo "CheCluster QuayEcosystem created!"
-
+echo "QuayEcosystem created!"
+echo " "
 echo "Install Quay Security Operator"
-oc apply -f quay-security.yaml
+oc apply -f quay-security-sub.yaml
 echo "Quay Security Operator installed!"
-
-echo "Searching for available routes"
-oc get routes -n quay
-echo "connect to the route named quay using your browser \
-and login using quay\password credentials"
+echo " "
+#echo "Searching for available routes"
+#oc get routes -n quay
+#echo "connect to the route named quay using your browser \
+#and login using quay\password credentials"
