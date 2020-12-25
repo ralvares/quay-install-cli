d699a2732b9994298f33cc35807d207961d45649
adnan.drina <adnan.drina@gmail.com>
2020-12-25 11:10:40 +0100

    wait for condition added

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo " "
 echo "Creating Quay Subscription"
 oc apply -f quay-sub.yaml
 sleep 30
-QUAY="$(oc get pods -o name -n quay | grep quay-operator)"
+QUAY="$(oc get subs -o name -n quay | grep quay-operator)"
 oc -n quay wait --timeout=120s --for=condition=CatalogSourcesUnhealthy=False ${QUAY}
 echo "Quay Subscription created!"
 echo " "
